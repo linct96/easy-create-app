b20543c84a7f5deeae9f082470c1e2d8aef8a83a
linct96 <314220356@qq.com>
2021-08-25 14:38:15 +0800

    reinit

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { hot } from "react-hot-loader/root";
-import ReactDOM from "react-dom";
-import App from './App';
-// import 'antd/dist/antd.css'
-import "tailwindcss/tailwind.css"
-
-const HotApp = hot(App)
-ReactDOM.render(<HotApp />, document.getElementById("app"));
